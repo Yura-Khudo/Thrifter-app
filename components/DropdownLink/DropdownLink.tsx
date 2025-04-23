00fb3a8b84c696a53fb09d2447b5407ab964157d
Yura <yrikhudyo@gmail.com>
2025-04-23 20:50:34 +0300

    Createcting DropdownLink component and it's functionality

---
+++ components/DropdownLink/DropdownLink.tsx
@@ -0,0 +1,39 @@
+import classes from "./DropdownLink.module.css";
+import Link from "next/link";
+
+const DropdownLink: React.FC<{
+	setContent: React.Dispatch<
+		React.SetStateAction<{ arr: string[]; isOpen: boolean }>
+	>;
+	content: { arr: string[]; isOpen: boolean };
+	arr: string[];
+	link: string;
+	title: string;
+}> = ({ setContent, content, arr, link, title }) => {
+	return (
+		<div
+			onMouseLeave={() =>
+				setContent((prevState) => {
+					return { ...prevState, isOpen: false };
+				})
+			}
+			onMouseOver={() => setContent({ arr, isOpen: true })}
+			className={`${classes.dropdownLink} `}
+		>
+			<Link
+				className={`${classes.link} ${
+					content.isOpen && content.arr === arr && classes.activeLink
+				}`}
+				href={`/${link}`}
+			>
+				{title}
+				<p
+					className={`${classes.arrow} ${
+						content.isOpen && content.arr === arr && classes.activeArrow
+					}`}
+				></p>
+			</Link>
+		</div>
+	);
+};
+export default DropdownLink;
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,46 +1,65 @@
-import Link from "next/link";
+"use client";
+
 import classes from "./Navbar.module.css";
 import { sizeOfPants, sizeOfShoes, typesOfClothes } from "@/utils/arrUtils";
+import DropdownLink from "../DropdownLink/DropdownLink";
+import { useState } from "react";
 
 const Navbar: React.FC = () => {
+	const [content, setContent] = useState({ arr: sizeOfPants, isOpen: false });
+	// const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<div className={classes.main}>
-			<div className={classes.navContainer}>
-				<div className={classes.nav}>
-					<div className={`${classes.dropdownLink} `}>
-						<Link className={classes.link} href="/man">
-							For Men
-							<p className={classes.arrow}></p>
-						</Link>
-						<div className={classes.menu}>
-							{typesOfClothes.map((el) => (
-								<p key={el}>{el}</p>
-							))}
-						</div>
-					</div>
-					<div className={`${classes.dropdownLink} `}>
-						<Link className={classes.link} href="/man">
-							For Woman
-							<p className={classes.arrow}></p>
-						</Link>
-						<div className={classes.menu}>
-							{sizeOfPants.map((el) => (
-								<p key={el}>{el}</p>
-							))}
-						</div>
-					</div>
-					<div className={`${classes.dropdownLink} `}>
+		<div className={classes.navContainer}>
+			<div className={classes.nav}>
+				<div
+					onMouseLeave={() =>
+						setContent((prevState) => {
+							return { ...prevState, isOpen: false };
+						})
+					}
+					onMouseOver={() =>
+						setContent((prevState) => {
+							return { ...prevState, isOpen: true };
+						})
+					}
+					className={`${classes.menu} ${content.isOpen && classes.open}`}
+				>
+					{content.arr.map((el) => (
+						<p key={el}>{el}</p>
+					))}
+				</div>
+				<DropdownLink
+					setContent={setContent}
+					content={content}
+					arr={sizeOfShoes}
+					link="men"
+					title="FOR MEN"
+				/>
+				<DropdownLink
+					setContent={setContent}
+					content={content}
+					arr={sizeOfPants}
+					link="women"
+					title="FOR WOMEN"
+				/>
+				<DropdownLink
+					setContent={setContent}
+					content={content}
+					arr={typesOfClothes}
+					link="unisex"
+					title="UNISEX"
+				/>
+				{/* <div
+						onMouseLeave={close}
+						onMouseOver={() => openMenu(typesOfClothes)}
+						className={`${classes.dropdownLink} `}
+					>
 						<Link className={classes.link} href="/man">
 							UNISEX
 							<p className={classes.arrow}></p>
 						</Link>
-						<div className={classes.menu}>
-							{sizeOfShoes.map((el) => (
-								<p key={el}>{el}</p>
-							))}
-						</div>
-					</div>
-				</div>
+					</div> */}
 			</div>
 		</div>
 	);
