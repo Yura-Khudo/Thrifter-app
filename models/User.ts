8c8317fdcabb7f30966098bbfa289193cd9f7a44
Yura <yrikhudyo@gmail.com>
2025-05-21 10:25:00 +0300

    User Schema

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+export interface UserInt extends mongoose.Document {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+}
+
+const UserSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
