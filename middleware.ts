f0215a38921a08178067248f266101c4f67917f5
Yura <yrikhudyo@gmail.com>
2025-06-03 11:18:31 +0300

    middleware: redirect when user not logged in

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,24 @@
+import { deleteSession, verifyUser } from "./lib/session";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
+	const path = req.nextUrl.pathname;
+	const user = await verifyUser();
+
+	if (path.startsWith("/logout")) {
+		await deleteSession();
+		return NextResponse.redirect(new URL("/", req.url));
+	}
+
+	if (
+		(path.startsWith("/myaccount") && !user) ||
+		(path.startsWith("/sell") && !user)
+	) {
+		return NextResponse.redirect(new URL("/login", req.url));
+	}
+
 	return NextResponse.next();
 }
 
